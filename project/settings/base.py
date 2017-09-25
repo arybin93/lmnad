301d1d5c149dd2f0269763ecc350db4027658517
Artem Rybin <arybin93@gmail.com>
2017-09-25 13:42:35 +0300

    Feature - set supporting zip format

--- project/settings/base.py
+++ project/settings/base.py
@@ -177,6 +177,14 @@ ABSOLUTE_URL_OVERRIDES = {
 # file browser
 FILEBROWSER_SUIT_TEMPLATE = True
 FILEBROWSER_DIRECTORY = 'uploads/'
+FILEBROWSER_MAX_UPLOAD_SIZE = 104857600
+
+FILEBROWSER_EXTENSIONS = {
+    'Image': ['.jpg', '.jpeg', '.gif', '.png', '.tif', '.tiff'],
+    'Document': ['.pdf', '.doc', '.rtf', '.txt', '.xls', '.csv', '.zip'],
+    'Video': ['.mov', '.wmv', '.mpeg', '.mpg', '.avi', '.rm'],
+    'Audio': ['.mp3', '.mp4', '.wav', '.aiff', '.midi', '.m4p']
+}
 
 CKEDITOR_UPLOAD_PATH = "uploads/users/photo"
 CKEDITOR_IMAGE_BACKEND = "pillow"
