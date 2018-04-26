deb8aa84ad745e854bb4651f7b7ead69841cf5e1
Maria Kokoulina <kokoulinamaria97@gmail.com>
2018-04-26 15:41:05 +0300

    Bugfix - http to https

--- igwatlas/static/igwatlas/igwatlas.js
+++ igwatlas/static/igwatlas/igwatlas.js
@@ -24,7 +24,7 @@ function init () {
      myMap.geoObjects.add(objectManager);
 
      $.ajax({
-        url: "http://lmnad.nntu.ru/api/v1/records/?api_key=d837d31970deb03ee35c416c5a66be1bba9f56d3"
+        url: "https://lmnad.nntu.ru/api/v1/records/?api_key=d837d31970deb03ee35c416c5a66be1bba9f56d3"
      }).done(function(data) {
         objectManager.add(data);
      });
@@ -53,7 +53,7 @@ function init () {
 
         // send request with params
         $.ajax({
-            url: "http://lmnad.nntu.ru/api/v1/records/?api_key=",
+            url: "https://lmnad.nntu.ru/api/v1/records/?api_key=",
             type: 'get',
             data: {
                 api_key: 'd837d31970deb03ee35c416c5a66be1bba9f56d3',
@@ -90,7 +90,7 @@ function init () {
 
     function reset_request() {
         $.ajax({
-            url: "http://lmnad.nntu.ru/api/v1/records/?api_key=d837d31970deb03ee35c416c5a66be1bba9f56d3"
+            url: "https://lmnad.nntu.ru/api/v1/records/?api_key=d837d31970deb03ee35c416c5a66be1bba9f56d3"
         }).done(function(data) {
             myMap.geoObjects.removeAll();
             objectManager.removeAll();
