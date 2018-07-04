836c5f6a1060a97611cf3698c8550b1468c29091
Artem Rybin <arybin93@gmail.com>
2018-07-04 15:14:12 +0300

    Bugfix - requirements

--- project/celeryconfig.py
+++ project/celeryconfig.py
@@ -3,5 +3,5 @@ result_backend = 'rpc://'
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
-timezone = 'Europe/Moscow'
-enable_utc = True
+#timezone = 'Europe/Moscow'
+#enable_utc = True
