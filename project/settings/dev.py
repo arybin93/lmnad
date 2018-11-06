c47174596ee313a33a2730d4ee6eaee2da8642cb
Artem Rybin <arybin93@gmail.com>
2018-11-06 18:20:36 +0300

    Support - Dockerize app for development

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -6,15 +6,15 @@ SECRET_KEY = 'e^gzvyq8(f!5kg5o*(&a3!j(#0ky)80&8bd+_7i!i^7*3lbbno'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'lmnad_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
+        'USER': 'dev',
+        'PASSWORD': 'dev',
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
