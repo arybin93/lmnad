8d83d1858c2b68b6009dd3faab6d7039086d0628
tmoiseenko <tanya.moskaleva.99@mail.ru>
2020-06-25 21:06:58 +1100

    fix - add corrections

--- igwatlas/models.py
+++ igwatlas/models.py
@@ -167,7 +167,10 @@ class WaveData(models.Model):
     record = models.ForeignKey(Record, on_delete=models.CASCADE, verbose_name='Запись для анализа')
 
     def __str__(self):
-        return self.get_type_display()
+        if self.type:
+            return self.get_type_display()
+        if self.polarity:
+            return self.get_polarity_display()
 
     class Meta:
         verbose_name = 'Параметры'
