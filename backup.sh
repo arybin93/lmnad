dc0e81fdcc90f0997f000743532927f7c5b191fa
ivanchainik <vanya992008@gmail.com>
2021-03-31 13:28:59 +0300

    Change script for backup

--- backup.sh
+++ backup.sh
@@ -6,10 +6,5 @@ mysql_password=root
 
 now=$(date +"%d_%m_%Y")
 
-$mysqldump --user=$mysql_user  --password=$mysql_password --databases lmnad_db | gzip > backup_$now.sql.gz
+docker exec -i lmnad_mysql_dev $mysqldump --user=$mysql_user  --password=$mysql_password --databases lmnad_db | gzip > backup/backup_$now.sql.gz
 
-exit
-
-docker cp lmnad_mysql_dev:backup_$now.sql.gz /home/ivan/lmnad-master/backup
-
-docker exec -ti lmnad_mysql_dev rm backup_$now.sql.gz
