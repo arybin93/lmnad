aa9e352d6c32048242359f2a96391b0b7616f243
Artem Rybin <arybin93@gmail.com>
2021-08-06 19:43:55 +0300

    Hotfix - fix changing language

--- tank/urls.py
+++ tank/urls.py
@@ -7,6 +7,6 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path('tank_exp/', tank, name='tank_exp'),
-    path('tank_exp/<int:pk>/', tank_exp_detail),
+    path('tank_exp/<int:pk>/', tank_exp_detail, name='tank_exp_detail'),
     path('tank_exp_about/', tank, name='tank_exp_about'),
 ]
